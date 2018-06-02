00e1f07e75c9cbb554dc54f3026057d5655a33ac
Hendrik Wiese <github@hwiese.com>
2018-06-02 12:44:01 +0200

    Non central Z-homing point

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1106,8 +1106,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT (15)    // X point for Z homing when homing all axes (G28).
-  #define Z_SAFE_HOMING_Y_POINT (15)    // Y point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_X_POINT (50)    // X point for Z homing when homing all axes (G28).
+  #define Z_SAFE_HOMING_Y_POINT (50)    // Y point for Z homing when homing all axes (G28).
 #endif
 
 // Homing speeds (mm/m)
